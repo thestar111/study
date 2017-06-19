7bec3b048a94baf684a668e4bfb720e5271ae8ae
HidalgoWolves <zhouping19911013@163.com>
2017-06-19 10:09:16 +0800

    Update debug.sh

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -1,6 +1,7 @@
+# author zhouping
+# linux tomcat debug scripts
 #!/bin/bash
 # file: $CATALINA_HOME/bin/debug.sh
-# author zhouping
 export JPDA_DEBUG="-Xdebug -Xrunjdwp:transport=dt_socket,address=8879,server=y,suspend=n"
 export JAVA_OPTS="$JAVA_OPTS $JPDA_DEBUG"
 ./startup.sh; tail -f ../logs/catalina.out
